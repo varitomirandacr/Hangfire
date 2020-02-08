3f9ed91ffcc89d7982afea8075f51f97480a17a5
Alvaro <varitomiranda@gmail.com>
2020-02-08 02:46:28 -0600

    Added hangfire configuration and initial dashboard

--- HangfireAPI/Startup.cs
+++ HangfireAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Hangfire;
+using Hangfire.Mongo;
+using MongoDB.Driver;
 
 namespace HangfireAPI
 {
@@ -26,15 +29,53 @@ namespace HangfireAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var mongoConnection = Configuration.GetConnectionString("HangfireConnection");
+            var migrationOptions = new MongoMigrationOptions
+            {
+                Strategy = MongoMigrationStrategy.Drop,
+                BackupStrategy = MongoBackupStrategy.Collections
+            };
+
+            // Add Hangfire services.
+            services.AddHangfire(config =>
+            {
+                config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170);
+                config.UseSimpleAssemblyNameTypeSerializer();
+                config.UseRecommendedSerializerSettings();
+                config.UseMongoStorage(new MongoClient(mongoConnection), "Hangfire", new MongoStorageOptions { MigrationOptions = migrationOptions });
+            });
+
+            // SQL settings
+            //.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            //.UseSimpleAssemblyNameTypeSerializer()
+            //.UseRecommendedSerializerSettings()
+            //.UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+            //{
+            //    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+            //    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+            //    QueuePollInterval = TimeSpan.Zero,
+            //    UseRecommendedIsolationLevel = true,
+            //    UsePageLocksOnDequeue = true,
+            //    DisableGlobalLocks = true
+            //}));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IBackgroundJobClient backgroundJobs)
+        {            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseHangfireDashboard();
+            backgroundJobs.Enqueue(() => Console.WriteLine("Access the Dashboard at: https://localhost:5001/hangfire"));
 
             app.UseHttpsRedirection();
 
